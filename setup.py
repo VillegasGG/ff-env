55860848e2b86e54b201b8182a6efa76c7ae2dbc
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-07-14 23:01:22 -0600

    update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
+    py_modules=["candidates_utils", "fire_state", "firefighter", "tree_generator", "tree_utils"],
     install_requires=[
         "gymnasium",
         "numpy"
